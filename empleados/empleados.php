c26cdfc47537afed1bef80ffcabcd8005908fd6e
LoiLoL421 <j.robleslecona@ugto.mx>
2024-03-14 09:24:05 -0600

    Se han realizado algunas correciones al código para que funcione pero aun no funciona unu

--- empleados/empleados.php
+++ empleados/empleados.php
@@ -1,12 +1,12 @@
 <?php
-    include('../conexion/conexion.php')
+    include('../conexion/conexion.php');
 
     $txtID = (isset($_POST['txtID'])) ? $_POST['txtID']: '';
     $txtNombre = (isset($_POST['txtNombre'])) ? $_POST['txtNombre']: '';
     $txtApaterno = (isset($_POST['txtApaterno'])) ? $_POST['txtApaterno']: '';
     $txtAmaterno = (isset($_POST['txtAmaterno'])) ? $_POST['txtAmaterno']: '';
     $txtCorreo = (isset($_POST['txtCorreo'])) ? $_POST['txtCorreo']: '';
-    $txtFoto = (isset($_POST['txtFoto'])) ? $_POST['txtFoto']: '';
+    $txtFoto = (isset($_FILES['txtFoto']['name'])) ? $_FILES['txtFoto']['name']: '';
     $accion = (isset($_POST['accion'])) ? $_POST['accion']: '';
 
     $error = array();
@@ -14,8 +14,8 @@
     $accionAgregar = '';
     $accionModificar = $accionEliminar = $accionCancelar = "disabled";
     $mostrarModal = false;
-    swicth ($accion) {
-        case 'btnAgregar';
+    switch ($accion) {
+        case 'btnAgregar':
             if ($txtNombre == ''){
             $error ['Nombre'] = 'nombre no puede ir vacio';
             }
@@ -56,21 +56,70 @@
             break;
         
             case 'btnEliminar':
-                $queryEliminar = $pdo->prepare('SELECT foro FROM empleados WHERE id=:id')
+                $queryEliminar = $pdo->prepare('SELECT foto FROM empleados WHERE id=:id');
                 $queryEliminar->bindParam(':id', $txtID);
                 $queryEliminar->execute();
                 $empleado = $queryEliminar->fecth(PDO::FETCH_LAZY);
 
                 if(isset($empleado['foto']) && $empleado['foto'] != 'image') {
                     if (file_exists('../imagenes/'. $empleado['foto'])) {
-                        unlink('../imagenes/'.$empleado['foto'])
+                        unlink('../imagenes/'.$empleado['foto']);
                     }
                 }
 
             $queryDelete = $pdo->prepare('DELETE FROM empleados WHERE id=:id');
             $queryDelete->bindParam(':id', $txtID);
             header('location: index.php');
+            break;
 
+            case 'btnModificar':
+                    $queryUpdate = $pdo->prepare("UPDATE empleados SET nombre =:nombre, 
+                    apaterno= :apaterno, amaterno=:amaterno, correo= :correo WHERE id=:id");
+                $queryUpdate->bindParam(':nombre', $txtNombre);
+                $queryUpdate->bindParam(':apaterno', $txtApaterno);
+                $queryUpdate->bindParam(':amaterno', $txtAmaterno);
+                $queryUpdate->bindParam(':correo', $txtCorreo);
+                $queryUpdate->bindParam(':id', $txtID);
+                $queryUpdate->execute();
+                $fecha = new DateTime();
+                $nombreFoto = ($txtFoto != '') ? $fecha->getTimestamp() . "_" . $__FILES["txtFoto"]["name"] : "image";
+                $tmpFoto = $_FILES ['txtFoto']['tmp_name'];
+                if ($tmpFoto != '') {
+                    move_uploaded_file($tmpFoto, "../imagenes/".$nombreFoto);
+                $queryEliminar = $pdo->prepare('SELECT foto FROM empleados WHERE id=:id');
+                $queryEliminar->bindParam(':id', $txtID);
+                $queryEliminar->execute();
+                $empleado = $queryEliminar->fecth(PDO::FETCH_LAZY);
+
+                if(isset($empleado['foto']) && $empleado['foto'] != 'image') {
+                    if (file_exists('../imagenes/'. $empleado['foto'])) {
+                        unlink('../imagenes/'.$empleado['foto']);
+                    }
+                    }
+                    $queryUpdateFoto = $pdo -> prepare('UPDATE empleados SET foto=:foto WHERE id=:id');
+                    $queryUpdateFoto->bindParam(':foto', $nombreFoto);
+                    $queryUpdateFoto->bindParam(':id', $txtID);
+                }
+                header('Location: index.php');
+            break;
+
+            case 'btnCancelar':
+                header('Location: index.php');
+            break;
+            case 'Seleccionar':
+                    $accionAgregar = "disabled";
+                    $accionModificar = $accionEliminar = $accionCancelar = '';
+                    $mostrarModal = true;
+                    $queryEmpleado = $pdo->prepare('SELECT * FROM empleados WHERE id=:id');
+                    $queryEmpleado->bindParam(':id', $txtID);
+                    $queryEmpleado->execute();
+                    $empleado = $queryEmpleado ->fetch(PDO::FETCH_LAZY);
+                    $txtNombre = $empleado['nombre'];
+                    $txtApaterno = $empleado['apaterno'];
+                    $txtAmaterno = $empleado['apaterno'];
+                    $txtCorreo = $empleado['correo'];
+                    $txtFoto = $empleado['foto'];
+            break;
     }
 
     $querySelect = $pdo->prepare('SELECT * FROM empleados');
